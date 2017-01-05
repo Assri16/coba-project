521d32cfcb50ead2a57c753147e3e5368c935f21
TOSHIBA-PC\TOSHIBA <ardhiapradita@gmail.com>
2017-01-05 14:53:08 +0700

    edit laporan

--- admin/lap_barang.php
+++ admin/lap_barang.php
@@ -8,7 +8,7 @@ $pdf->SetMargins(2,1,1);
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Times','B',11);
-$pdf->Image('../admin/foto/Polije.png',1,1,2,2);
+$pdf->Image('../admin/foto/tokodata.png',1,1,2,2);
 $pdf->SetX(4);            
 $pdf->MultiCell(19.5,0.5,'MIF A Polije',0,'L');
 $pdf->SetX(4);
